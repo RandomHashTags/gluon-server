7218606b0346e3bce375f292ba66b9bed708ce7b
RandomHashTags <ImRandomHashTags@gmail.com>
2022-11-03 12:05:06 -0500

    Initial Commit

---
+++ quark/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  quark
+//
+//  Created by Evan Anderson on 11/3/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
